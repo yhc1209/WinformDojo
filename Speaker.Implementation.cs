14296109f24bdde69d01213dd24fb4aaa0631c01
yhc <abcd83129@gmail.com>
2026-09-14 22:10:10 +0800

    定義Speaker。

---
+++ Speaker.Implementation.cs
@@ -0,0 +1,37 @@
+using System.Security.Cryptography;
+
+namespace WinformDojo;
+
+public class Dog : ISpeaker
+{
+    public string Name { get; } = "Dog";
+
+    private static readonly string[] SPEECHES = {
+        "Want.", "Want~", "Want, want.", "Want...", "WANT!",
+        "Want!", "Want! want!", "Want! want! want!",
+        "Want! Want and want!", "Want! Want and want! ...want?!",
+        "Want! Want! Want! Want and want! ...want?!",
+        "Want! Want! Want! Want! Want! Want!", "WAAAAAAAAAAANNT!!!!"
+    };
+
+    public string Speak()
+    {
+        int idx = RandomNumberGenerator.GetInt32(SPEECHES.Length);
+        return SPEECHES[idx];
+    }
+}
+
+public class Cat : ISpeaker
+{
+    public string Name { get; } = "Cat";
+
+    private static readonly string[] SPEECHES = {
+        "Meow~", "Meow.", "Meeeoow...", "Meow, meow..."
+    };
+    
+    public string Speak()
+    {
+        int idx = RandomNumberGenerator.GetInt32(SPEECHES.Length);
+        return SPEECHES[idx];
+    }
+}
