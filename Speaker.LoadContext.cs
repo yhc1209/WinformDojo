366b6a6c17316f168aa90e6eb5c86f40b217df5c
yhc <abcd83129@gmail.com>
2026-09-14 22:12:20 +0800

    定義pkugin load context。

---
+++ Speaker.LoadContext.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace WinformDojo;
+
+public class SpeakerLoadContext : AssemblyLoadContext
+{
+    private readonly AssemblyDependencyResolver _resolver;
+
+    public SpeakerLoadContext(string dllpath) : base(isCollectible: true)
+    {
+        _resolver = new AssemblyDependencyResolver(dllpath);
+    }
+
+    protected override Assembly Load(AssemblyName assemblyName)
+    {
+        string assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+        return assemblyPath != null ? LoadFromAssemblyPath(assemblyPath) : null;
+    }
+}
