23e93674a5bd6d83de65ca42e1432873be661270
How <How@TrustONE.com.tw>
2026-09-15 12:33:25 +0800

    LoadContext搬移。

--- WinformDojo/Plugin/SpeakerLoadContext.cs
+++ WinformDojo/Plugin/SpeakerLoadContext.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.Loader;
 
-namespace SharedContract.Speaker;
+namespace WinformDojo.Plugin;
 
 public class SpeakerLoadContext : AssemblyLoadContext
 {
